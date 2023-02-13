bd1cdf674d5dc89bbb91a48d450b0ee21eb0bdce
Mahmoud Easa <89482509+MahmoudEasa@users.noreply.github.com>
2023-02-13 00:24:04 +0000

    Change all if conditions to one in function

--- ProgrammingInC2.4[Arabic].c
+++ ProgrammingInC2.4[Arabic].c
@@ -22,7 +22,8 @@ https://ideone.com/12nmNY
 
 // int greaterNumOf6Nums(int num1, int num2, int num3, int num4, int num5,
 //                       int num6) {
-//   if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5 && num1 > num6) {
+//   if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5 && num1 >
+//   num6) {
 //     return num1;
 //   } else if (num2 > num3 && num2 > num4 && num2 > num5 && num2 > num6) {
 //     return num2;
@@ -39,7 +40,8 @@ https://ideone.com/12nmNY
 
 // void printResult(int greater_num, int num1, int num2, int num3, int num4,
 //                  int num5, int num6) {
-//   printf("The greater number of [ %d, %d, %d, %d, %d, %d ] is %d", num1, num2,
+//   printf("The greater number of [ %d, %d, %d, %d, %d, %d ] is %d", num1,
+//   num2,
 //          num3, num4, num5, num6, greater_num);
 // }
 
@@ -54,3 +56,78 @@ https://ideone.com/12nmNY
 
 //   return 0;
 // }
+
+//
+//
+//
+//
+//
+//
+//
+
+// int greaterNumOf6Nums(int num1, int num2, int num3, int num4, int num5,
+//                       int num6) {
+//   int greaterNum =
+//       num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5 && num1 > num6
+//           ? num1
+//       : num2 > num3 && num2 > num4 && num2 > num5 && num2 > num6 ? num2
+//       : num3 > num4 && num3 > num5 && num3 > num6                ? num3
+//       : num4 > num5 && num4 > num6                               ? num4
+//       : num5 > num6                                              ? num5
+//                                                                  : num6;
+
+//   return greaterNum;
+// }
+//
+//
+//
+//
+//
+//
+//
+// Other Solution
+
+// https://ideone.com/KviGAH
+
+void read6Nums(int *num1, int *num2, int *num3, int *num4, int *num5,
+               int *num6) {
+  printf("Please Enter 6 Numbers:\n");
+  scanf("%d %d %d %d %d %d", num1, num2, num3, num4, num5, num6);
+}
+
+int greaterNumOf2Nums(int num1, int num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+int greaterNumOf6Nums(int num1, int num2, int num3, int num4, int num5,
+                      int num6) {
+  int number1 = greaterNumOf2Nums(num1, num2);
+  int number2 = greaterNumOf2Nums(num3, num4);
+  int number3 = greaterNumOf2Nums(num5, num6);
+  int number4 = greaterNumOf2Nums(number1, number2);
+  int number5 = greaterNumOf2Nums(number3, number4);
+
+  return number5;
+}
+
+void printResult(int greater_num, int num1, int num2, int num3, int num4,
+                 int num5, int num6) {
+  printf("The greater number of [ %d, %d, %d, %d, %d, %d ] is %d", num1, num2,
+         num3, num4, num5, num6, greater_num);
+}
+
+int main() {
+  int num1, num2, num3, num4, num5, num6, greater_num;
+
+  read6Nums(&num1, &num2, &num3, &num4, &num5, &num6);
+
+  greater_num = greaterNumOf6Nums(num1, num2, num3, num4, num5, num6);
+
+  printResult(greater_num, num1, num2, num3, num4, num5, num6);
+
+  return 0;
+}
