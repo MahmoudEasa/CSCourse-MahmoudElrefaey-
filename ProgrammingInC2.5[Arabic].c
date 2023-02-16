1bf4d252a06e899b9d797adde99d3ba0b2ef001d
Mahmoud Easa <89482509+MahmoudEasa@users.noreply.github.com>
2023-02-16 10:57:23 +0000

    RecursionNumPowerNum

---
+++ ProgrammingInC2.5[Arabic].c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Mahmoud Easa
+
+/*
+https://ideone.com/fsW27v
+
+  Recursion:
+    Read Num
+    Read powerInput
+    Return Num power powerInput
+    Print Result
+*/
+
+void readNumAndPower(int *num, int *powerInput) {
+  printf("Please Enter Number: ");
+  scanf("%d", num);
+
+  printf("Please Enter Power: ");
+  scanf("%d", powerInput);
+}
+
+int powerN(int num, int powerInput) {
+  return powerInput > 1 ? num * powerN(num, powerInput - 1) : num;
+}
+
+void printResult(int num, int powerInput) {
+  printf("%d Power %d = ( %d )", num, powerInput, powerN(num, powerInput));
+}
+
+int main() {
+  int num, powerInput;
+  readNumAndPower(&num, &powerInput);
+  printResult(num, powerInput);
+
+  return 0;
+}
